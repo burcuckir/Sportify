0b40971dea7ef0d353442ef7cbfd087000b34933
burcucakir <burcuckir@gmail.com>
2024-12-16 19:26:01 +0300

    add projects

---
+++ gateway/src/main/java/com/example/gateway/filters/JwtAuthFilter.java
@@ -0,0 +1,80 @@
+package com.example.gateway.filters;
+
+import io.jsonwebtoken.JwtException;
+import io.jsonwebtoken.Jwts;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.cloud.gateway.route.Route;
+import org.springframework.cloud.gateway.support.ServerWebExchangeUtils;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.server.reactive.ServerHttpRequest;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+import java.util.Date;
+import java.util.Objects;
+import java.util.UUID;
+
+@Component
+public class JwtAuthFilter implements GlobalFilter {
+
+    @Value("${jwt.secret}")
+    private String secretKey;
+
+    private static final Logger logger = LoggerFactory.getLogger(JwtAuthFilter.class);
+
+    @Override
+    public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+
+        Route route = exchange.getAttribute(ServerWebExchangeUtils.GATEWAY_ROUTE_ATTR);
+        if (Objects.equals(route.getMetadata().get("requiresAuth").toString(), "false")) {
+            return chain.filter(exchange);
+        }
+
+
+        String authHeader = exchange.getRequest().getHeaders().getFirst(HttpHeaders.AUTHORIZATION);
+        if (!authHeader.startsWith("Bearer ")) {
+            exchange.getResponse().setStatusCode(HttpStatus.UNAUTHORIZED);
+            return exchange.getResponse().setComplete();
+        }
+
+        String token = authHeader.substring(7);
+        try {
+            // Token doğrulama (ör. JJWT kütüphanesi veya nimbus-jose-jwt ile)
+
+
+            var claims = Jwts.parser().setSigningKey(secretKey).parseClaimsJws(token);
+
+            // Daha detaylı kontrol (exp, roles, vs.)
+            Date expiration = claims.getBody().getExpiration();
+            if (expiration.before(new Date())) {
+                throw new JwtException("Token expired");
+            }
+
+            UUID userId = UUID.fromString(claims.getBody().getId());
+
+            // Yeni bir ServerHttpRequest oluşturup x-user-id ekliyoruz
+            ServerHttpRequest mutatedRequest = exchange.getRequest().mutate()
+                    .header("x-user-id", userId.toString())
+                    .build();
+
+            // Yeni Request'i içeren Exchange nesnesi
+            ServerWebExchange mutatedExchange = exchange.mutate()
+                    .request(mutatedRequest)
+                    .build();
+
+            // Filtre zincirine, değiştirilmiş Exchange ile devam ediyoruz
+            return chain.filter(mutatedExchange);
+
+        } catch (Exception e) {
+            logger.error("Token validation error: {}", e.getMessage());
+            exchange.getResponse().setStatusCode(HttpStatus.UNAUTHORIZED);
+            return exchange.getResponse().setComplete();
+        }
+    }
+}
