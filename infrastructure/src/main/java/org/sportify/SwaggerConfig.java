9bd6d76935da4e45944f5c31f984942995e157f4
burcucakir <burcuckir@gmail.com>
2024-12-21 00:09:29 +0300

    move SwaggerConfig to infrastructure

--- infrastructure/src/main/java/org/sportify/SwaggerConfig.java
+++ infrastructure/src/main/java/org/sportify/SwaggerConfig.java
@@ -1,4 +1,4 @@
-package com.sportify.reservationservice.configuration;
+package org.sportify;
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
